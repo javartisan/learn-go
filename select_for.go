b12529f7134868567388946b7d4fec7145ed55b6
javartisan <javartisan@163.com>
2022-06-07 13:40:34 +0800

    add go code

--- select_for.go
+++ select_for.go
@@ -4,13 +4,15 @@ import "fmt"
 
 func main() {
 
-	ch := make(chan int, 1)
+	ch := make(chan int, 2)
 
 	for i := 0; i < 10; i++ {
 		select {
 		case v := <-ch:
 			fmt.Println("v = ", v)
+			// 第一次写0
 		case ch <- i:
+			fmt.Println("wirte ", i)
 		}
 	}
 
