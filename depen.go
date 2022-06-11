dfc74e0929fc5c17222a66a368d35a56f23aed84
javartisan <javartisan@163.com>
2022-06-11 18:59:48 +0800

    add code

--- depen.go
+++ depen.go
@@ -2,6 +2,9 @@ package main
 
 import "github.com/javartisan/fuck-dep/v2"
 
+/**
+	https://blog.csdn.net/Dax1n/article/details/125237592?csdn_share_tail=%7B%22type%22%3A%22blog%22%2C%22rType%22%3A%22article%22%2C%22rId%22%3A%22125237592%22%2C%22source%22%3A%22Dax1n%22%7D&ctrtid=AAAEd
+*/
 func main() {
 	fuck.Fuck()
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module learn-go
 go 1.15
 
 require (
-	github.com/javartisan/fuck-dep/v2 v2.2.2
+	github.com/javartisan/fuck-dep/v2   v2.2.2
 	gopl.io v0.0.0-20211004154805-1ae3ec64947b
 )
